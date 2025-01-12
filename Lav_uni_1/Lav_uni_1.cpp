ed08b66919a67e90bb7fb4f3b9f8cf380d8b23ab
AndryGamerTV <asdaartefret@gmail.com>
2025-01-12 19:30:04 +0200

    Add project files.

---
+++ Lav_uni_1/Lav_uni_1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+double f(double x, double eps)
+{
+	if (x <= -2 + eps)
+	{
+		return (1.0 / 4) * pow(x, 3) - x + 5;
+	}
+	if (x > -2 && x < 2);
+	{
+		return (x * asin(x / 2)) / (x * x + 1);
+	}
+	return exp(abs(cos(x))) - log(pow(x, 2) + 1);
+}
+
+int main()
+{
+	double x = -2;
+
+	const double eps = 1e-8;
+
+	std::cout << "--- Task #1 ---" << std::endl;
+	std::cout << "Input x: "; std::cin >> x;
+	std::cout << "f(" << x << ") = " << std::fixed << std::setprecision(2) << f(x, eps) << std::endl;
+}
+
+/* --- Task-1 ---
+test_1
+input: x = -2, eps = 1e-8
+output: 5
+
+test_2
+input: x = 2, eps = 1e-8
+output: 0.628319
+
+test_3
+input: 1 eps = 1e-8
+output: 0.261799
+
+test_4
+input: 0 eps = 1e-8
+output: 0
+*/
